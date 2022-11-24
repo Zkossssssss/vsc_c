116a3d29e3b022009306c9e56fe15adc02037635
Konstantin Bobrovskiy <k_bobrovskiy03@mail.ru>
2022-11-24 11:58:12 +0700

    2211

---
+++ str.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+char tracks[][80] = {
+  "Gaz Rule"
+  "I Want It All (Clean)"
+  "Need me"
+  "Diamond"
+  "LOCATION"
+};
+
+void find_track(char search_for[])
+{
+  int i;
+  for (i = 0; i < 5; i++)
+  {
+    if (strstr(tracks[i], search_for))
+    {
+      printf("Песня номер %i: '%s\n'",i,tracks[i]);
+    }
+  }
+}
+
+int main() {
+  char search_for[80];
+  printf("Искать: ");
+  fgets(search_for, 80, stdin);
+  search_for[strlen(search_for) - 1] = '\0';
+  find_track(search_for);
+  return 0;
+}
